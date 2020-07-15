120113796943ec4abb986e7c996d069516e9e7b8
Melanie Kuo <kuo.melanie@gmail.com>
2020-07-14 17:16:39 -0700

    Initial Commit

---
+++ ScanNGo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ScanNGo
+//
+//  Created by Melanie Kuo on 7/14/20.
+//  Copyright © 2020 Melanie Kuo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScanNGo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ScanNGo
+//
+//  Created by Melanie Kuo on 7/14/20.
+//  Copyright © 2020 Melanie Kuo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
